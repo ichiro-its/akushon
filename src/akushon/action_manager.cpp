6dcca0971d97c814961b0cba6d8b9893d82a23d3
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2021-06-09 03:38:13 +0700

    feat: minor change

--- src/akushon/action_manager.cpp
+++ src/akushon/action_manager.cpp
@@ -152,6 +152,8 @@ void ActionManager::load_action_data(
   const std::vector<std::string> & action_names)
 {
   clear_action_list();
+  this->action_names = action_names;
+
   uint8_t id = 0;
   for (auto action_name : action_names) {
     std::string file_name = path + "/" + action_name + ".json";
