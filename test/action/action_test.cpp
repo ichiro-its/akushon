8f67f075ce8383df1ebbaa32041a82314f87d566
Alffaza <ridzkiraihanalfaza@gmail.com>
2022-03-12 23:17:17 +0700

    fix: fix error

--- test/action/action_test.cpp
+++ test/action/action_test.cpp
@@ -27,8 +27,8 @@
 
 TEST(ActionTest, ClassInitialization){
     akushon::Action action("test action");
-    ASSERT_EQ(action.get_name, "test action");
-    ASSERT_TRUE(action.get_poses.empty());
+    ASSERT_EQ(action.get_name(), "test action");
+    ASSERT_TRUE(action.get_poses().empty());
 }
 
 TEST(ActionTest, SetterTest){
@@ -36,7 +36,7 @@ TEST(ActionTest, SetterTest){
     action.set_name("new test action");
     action.set_start_delay(1.0);
     action.set_stop_delay(3.0);
-    ASSERT_EQ(action.get_name, "new test action");
-    ASSERT_EQ(action.get_start_delay, 1.0);
-    ASSERT_EQ(action.get_stop_delay, 3.0); 
+    ASSERT_EQ(action.get_name(), "new test action");
+    ASSERT_EQ(action.get_start_delay(), 1.0);
+    ASSERT_EQ(action.get_stop_delay(), 3.0); 
 }
